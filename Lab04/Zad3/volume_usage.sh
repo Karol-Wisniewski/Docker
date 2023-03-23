de2dd95ccc4f9625eef0ac0563eab87b55157501
Karol-Wisniewski <karol-wisniewski2001@wp.pl>
2023-03-23 20:53:26 +0100

    Fix

--- Lab04/Zad3/volume_usage.sh
+++ Lab04/Zad3/volume_usage.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-docker volume ls -q | while read volume; do
-    echo -n "$volume: "
-    mountpoint=$(docker volume inspect $volume | awk -F'[ ,]+' -v vol="$volume" '$0~vol{p=1}p&&/Mountpoint/{gsub(/\\/, "/", $2); print $2; exit}' FS=": " OFS=" ")
-    echo -n "$mountpoint: "
-    if [ -n "$mountpoint" ]; then
-        usage=$(fsutil volume diskfree "$mountpoint" | awk 'NR==2{print $3}')
-        echo "$usage"
-    else
-        echo "Not mounted"
-    fi
+volumes=$(docker volume ls --format '{{ .Mountpoint }}')
+
+for volume in $volumes
+do
+  disk_usage=$(df -h $volume | awk 'NR==2 {print $5}')
+
+  percentage=$(echo $disk_usage | tr -d '%')
+
+  echo "Volume $volume is $percentage% full"
 done
