2adfe1ff72fcdf82956f9cabccc3e25386c1cc61
Karol-Wisniewski <karol-wisniewski2001@wp.pl>
2023-03-27 08:48:19 +0200

    Finished zad4

--- Lab04/Zad4/script.sh
+++ Lab04/Zad4/script.sh
@@ -3,5 +3,5 @@
 read -p "Enter volume name: " volume_name
 read -s -p "Enter password: " password
 
-docker run -v $volume_name:/data -v C:/Users/user/Desktop/Docker/Lab04/Zad4:/output \
+docker run -v $volume_name:/data -v C:/Users/"Piotr Damrych"/Desktop/UCZELNIA/studia-archiwum/Docker/lab04/zad4:/output \
 ubuntu bash -c "apt-get update && apt-get install -y gnupg && tar -czvf /output/$volume_name.tar -C /data . && gpg --batch --symmetric --cipher-algo AES256 --passphrase $password /output/$volume_name.tar && gpg --batch --decrypt --passphrase $password /output/$volume_name.tar.gpg && tar -xvf /output/$volume_name.tar -C /output && rm /output/$volume_name.tar.gpg"
