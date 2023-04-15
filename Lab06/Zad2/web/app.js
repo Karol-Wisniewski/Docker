0a8974179da79950fe26a62928333ccb9df4baff
Karol-Wisniewski <karol-wisniewski2001@wp.pl>
2023-04-15 16:53:14 +0200

    Changed implementation of Zad2 from Lab06

--- Lab06/Zad2/web/app.js
+++ Lab06/Zad2/web/app.js
@@ -1,38 +1,19 @@
 const express = require('express');
-const mysql = require('mysql');
-
+const mongoose = require('mongoose');
 const app = express();
-const port = 3000;
 
-const db = mysql.createConnection({
-  host: 'db',
-  user: 'root',
-  password: 'mysecretpassword',
-  database: 'my_database',
-  port: 3306
+mongoose.connect('mongodb://mongodb:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('Error connecting to MongoDB:', err));
+
+app.use((req, res, next) => {
+    const dbStatus = mongoose.connection.readyState;
+    res.setHeader('DB-Status', dbStatus);
+    next();
 });
-
-function connectWithRetry() {
-  console.log('Trying to connect to the database...');
-  db.connect((err) => {
-    if (err) {
-      console.error('Failed to connect to the database. Retrying in 5 seconds...');
-      setTimeout(connectWithRetry, 5000);
-    } else {
-      console.log('Connected to the database!');
-    }
-  });
-}
-
-connectWithRetry();
 
 app.get('/', (req, res) => {
-  db.query('SELECT 1 + 1 AS solution', (err, results) => {
-    if (err) throw err;
-    res.send(`Hello World! The result of the query is ${results[0].solution}`);
-  });
+    res.send('Hello from API!');
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+app.listen(80, () => console.log('App listening on port 80'));
